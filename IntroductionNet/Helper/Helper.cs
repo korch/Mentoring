097a3bd438acb387158f6f66dadaaa965ee388d9
korch <e.sobalevsky@gmail.com>
2018-10-16 22:46:43 +0300

    Added Helper
    Added Helper

---
+++ IntroductionNet/Helper/Helper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using Helper.Properties;
+
+namespace HelperProject
+{
+    public class GreetingHelper
+    {
+        private const string DefaultHello = "Hello";
+        private string _sayHello;
+
+        public GreetingHelper()
+        {
+            FillDictionary();
+            _sayHello = DefaultHello;
+        }
+
+        public Dictionary<string, string> HelloWords { get; private set; }
+
+        public string Greeting(string name)
+        {
+            return $"{DateTime.Now.ToLocalTime()} {_sayHello}, {name}";
+        }
+
+        public void ChooseHelloType(string country)
+        {
+            if (string.IsNullOrEmpty(country)) { 
+                return;
+            }
+
+            _sayHello = HelloWords.First(h => h.Key.Equals(country)).Value;
+        }
+
+        private void FillDictionary()
+        {
+            var myResourceClass = new ResourceManager(typeof(Resources));
+            var resourceSet = myResourceClass.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+
+            HelloWords = new Dictionary<string, string>();
+            foreach (DictionaryEntry entry in resourceSet) {
+                HelloWords.Add(entry.Key.ToString(), entry.Value.ToString());
+            }
+        }
+    }
+}
---
+++ IntroductionNet/Helper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Helper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("Helper")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d213ed6-e274-4e10-bdae-601dcaa14098")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
